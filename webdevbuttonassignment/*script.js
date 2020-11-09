77b1ccf64d10ed695b566acff0ecde06433cae7a
katrinad2000 <katrinad2000@gmail.com>
2020-11-09 10:28:43 -0500

    Uploading work

---
+++ webdevbuttonassignment/*script.js
@@ -0,0 +1,89 @@
+let redbod = document.querySelector('body');
+let redText = document.querySelector('h3');
+let redbutt = document.querySelector("#red");
+redbutt.addEventListener('click', redfunc);
+function redfunc(){
+ redbod.style.backgroundColor= "#F78181";
+ redText.textContent = "RED. the color of passion and energy. drama, danger, desire and love. courage, strength and power. red is vibrant and exciting.";
+}
+
+let orbod = document.querySelector('body');
+let orText = document.querySelector('h3');
+let orbutt = document.querySelector("#orange");
+orbutt.addEventListener('click', orfunc);
+function orfunc(){
+ orbod.style.backgroundColor= "#F7BE81";
+ orText.textContent = "ORANGE. the color of enthusiasm, fascination and joy. of determination and wartmth. orange is the color of the extrovert."
+}
+
+let yelbod = document.querySelector('body');
+let yelText = document.querySelector('h3');
+let yelbutt = document.querySelector("#yellow");
+yelbutt.addEventListener('click', yelfunc);
+function yelfunc(){
+ yelbod.style.backgroundColor= "#fff88f";
+ yelText.textContent = "YELLOW. the color of optimism, freshness and positivity. happiness and youthfulness. yellow is the color of the enlightenment and illumination, but can also represent cowardice or trickery."
+}
+
+let grnbod = document.querySelector('body');
+let grnText = document.querySelector('h3');
+let grnbutt = document.querySelector("#green");
+grnbutt.addEventListener('click', grnfunc);
+function grnfunc(){
+ grnbod.style.backgroundColor= "#76c477";
+ grnText.textContent = "GREEN. the color of life, renewel and nature. of growth and health. green is the color of freshness and harmony but can also represent greed and jealousy."
+}
+
+let blubod = document.querySelector('body');
+let bluText = document.querySelector('h3');
+let blubutt = document.querySelector("#blue");
+blubutt.addEventListener('click', blufunc);
+function blufunc(){
+ blubod.style.backgroundColor= "#a2e8f2";
+ bluText.textContent = "BLUE. the color of trust, intelligence and wisdom. of serendipity and stability. blue is the color of healing and faith but can also depict sadness."
+}
+
+let viobod = document.querySelector('body');
+let vioText = document.querySelector('h3');
+let viobutt = document.querySelector("#violet");
+viobutt.addEventListener('click', viofunc);
+function viofunc(){
+ viobod.style.backgroundColor= "#c8a7f2";
+ vioText.textContent = "VIOLET. the color of royalty, nobility and luxury but aslo spirtuality and reflection. of compassion and self awareness. violet is the color of the introvert but can also represent arrogance."
+}
+
+let pnkbod = document.querySelector('body');
+let pnkText = document.querySelector('h3');
+let pnkbutt = document.querySelector("#pink");
+pnkbutt.addEventListener('click', pnkfunc);
+function pnkfunc(){
+ pnkbod.style.backgroundColor= "#fac5c6";
+ pnkText.textContent = "PINK. the color of sweetness, playfulness and charm. of femininity and tenderness. pink is the color romance and innocence but also of optimism and vulnerability."
+}
+
+let brnbod = document.querySelector('body');
+let brnText = document.querySelector('h3');
+let brnbutt = document.querySelector("#brown");
+brnbutt.addEventListener('click', brnfunc);
+function brnfunc(){
+ brnbod.style.backgroundColor= "#c4a96c";	
+ brnText.textContent = "BROWN. the color of the earth, simplicity and resiliance. of honesty and reassurance. brown is the color of warm and sincerity but can also denote frugality and inelegance."
+}
+
+let grybod = document.querySelector('body');
+let gryText = document.querySelector('h3');
+let grybutt = document.querySelector("#grey");
+grybutt.addEventListener('click', gryfunc);
+function gryfunc(){
+ grybod.style.backgroundColor= "#dedede";
+ gryText.textContent = "GREY. the color of compromise, neutrality and balance.it is often associated with depression, loss and dullness."
+}
+
+let blkbod = document.querySelector('body');
+let blkText = document.querySelector('h3');
+let blkbutt = document.querySelector("#black");
+blkbutt.addEventListener('click', blkfunc);
+function blkfunc(){
+ blkbod.style.backgroundColor= "#ffffff";
+ blkText.textContent = "BLACK. the color of mystery, fear and strength. of authority and formality. black often represents death and evil as well as seriousness and sophistication.";
+}
